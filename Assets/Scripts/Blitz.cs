4b4b22fdb23e66d6b6599fec7d445324f1941a1b
Unknown <jannik.jochim@gmx.de>
2019-01-26 16:36:57 +0100

    Gras, Colors, Lightning

---
+++ Assets/Scripts/Blitz.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blitz : MonoBehaviour
+{
+
+    public GameObject blitz;
+    public GameObject announcePoint;
+
+    public float waitMinTime = 0.1f, WaitMaxTime = 1f;
+    public float announceTime;
+    public float blitzTime;
+
+    void Start() {
+        blitz.SetActive(false);
+        announcePoint.SetActive(false);
+    }
+
+    void OnEnable() {
+        StartCoroutine(WaitForBlitz());
+    }
+
+
+    IEnumerator WaitForBlitz() {
+        float waitSec = Random.Range(waitMinTime, WaitMaxTime);
+        yield return new WaitForSeconds(waitSec);
+
+        StartCoroutine(AnnounceBlitz());
+    }
+
+    IEnumerator AnnounceBlitz() {
+
+        announcePoint.SetActive(true);
+        yield return new WaitForSeconds(announceTime);
+        announcePoint.SetActive(false);
+
+        StartCoroutine(Blitzen());
+    }
+
+    IEnumerator Blitzen() {
+        blitz.SetActive(true);
+        yield return new WaitForSeconds(blitzTime);
+        blitz.SetActive(false);
+
+        this.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/BlitzSpawn.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlitzSpawn : MonoBehaviour
+{
+    public float atSameTime = 2;
+    public float radiusOfEarth = 0.85f;
+    public Vector2 spawnAnge = new Vector2 (-20, -180);
+
+    [Header("Until Next Blitz")]
+    [Space]
+    public float waitMinTime = 2; 
+    public float WaitMaxTime = 3;
+
+    [Header("Pooling")]
+    [Space]
+    public GameObject pooledObject;
+    public int pooledAmount = 20;
+    List<GameObject> pooledObjects;
+
+    void Start(){
+        pooledObjects = new List<GameObject>();
+        for (int i = 0; i < pooledAmount; i++) {
+            GameObject obj = (GameObject)Instantiate(pooledObject);
+            obj.transform.parent = transform;
+            obj.SetActive(false);
+            pooledObjects.Add(obj);
+        }
+
+        StartCoroutine(ActivateBlitz());
+    }
+
+    IEnumerator ActivateBlitz() {
+        float waitSec = Random.Range(waitMinTime, WaitMaxTime);
+        yield return new WaitForSeconds(waitSec);
+
+        for (int a = 0; a < atSameTime; a++) {
+
+            for (int i = 0; i < pooledObjects.Count; i++) {
+                if (!pooledObjects[i].activeInHierarchy) {
+                    pooledObjects[i].SetActive(true);
+                    pooledObjects[i].transform.position = transform.position;
+
+                    // hier muss eine random Winkeldrehung rein
+                    pooledObjects[i].transform.transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, Random.Range(spawnAnge.x, spawnAnge.y));
+                    pooledObjects[i].transform.Translate(new Vector3(0, radiusOfEarth, 0));
+                    break;
+                }
+            }
+        }
+
+        StartCoroutine(ActivateBlitz());
+    }
+}
